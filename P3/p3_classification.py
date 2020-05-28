7152e804eb7f105078982baf5e47b6b113774e87
antcc <antoniocoin07@gmail.com>
2020-05-28 12:13:28 +0200

    Reordenación. Añade módulo de visualización

--- P3/p3_classification.py
+++ P3/p3_classification.py
@@ -19,8 +19,6 @@ Antonio Coín Castro. Grupo 3.
 #
 
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import cm
 from timeit import default_timer
 from enum import Enum
 
@@ -29,11 +27,13 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import (SelectFromModel, VarianceThreshold, SelectKBest,
     f_classif)
-from sklearn.model_selection import GridSearchCV, learning_curve
+from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import Lasso, LogisticRegression, SGDClassifier, RidgeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import plot_confusion_matrix
+
+from p3_visualization import (scatter_pca, scatter_pca_classes, confusion_matrix,
+    plot_feature_importance, plot_learning_curve, plot_class_distribution, plot_corr_matrix)
 
 #
 # PARÁMETROS GLOBALES
@@ -45,24 +45,16 @@ PATH = "datos/"
 SAVE_FIGURES = False
 IMG_PATH = "img/classification/"
 
-# Estrategias de selección de características
+#
+# PROBLEMA DE CLASIFICACIÓN EN OPTDIGITS
+#
+
 class Selection(Enum):
+    """Estrategia de selección de características."""
     PCA = 0
     LASSO = 1
     ANOVA = 2
     NONE = 3
-
-#
-# FUNCIONES AUXILIARES
-#
-
-def wait():
-    """Introduce una espera hasta que se pulse el intro.
-       Limpia el plot anterior."""
-
-    if not SAVE_FIGURES:
-        input("\n(Pulsa [Enter] para continuar...)\n")
-    plt.close()
 
 def read_data(filename):
     """Lee los datos de dichero y los separa en características y etiquetas."""
@@ -70,329 +62,11 @@ def read_data(filename):
     data = np.genfromtxt(filename, delimiter = ",", dtype = np.double)
     return data[:, :-1], data[:, -1]
 
-#
-# VISUALIZACIÓN
-#
-
-def scatter_plot(X, y, axis, ws = None, labels = None,
-                 title = None, figname = "", cmap = cm.tab10):
-    """Muestra un scatter plot de puntos etiquetados por clases,
-       eventualmente junto a varias rectas de separación.
-         - X: matriz de características de la forma [x1, x2].
-         - y: vector de etiquetas o clases.
-         - axis: nombres de los ejes.
-         - ws: lista de vectores 2-dimensionales que representan las rectas
-           (se asumen centradas).
-         - labels: etiquetas de las rectas.
-         - title: título del plot.
-         - figname: nombre para guardar la gráfica en fichero."""
-
-    # Establecemos tamaño, colores e información del plot
-    plt.figure(figsize = (8, 6))
-    plt.xlabel(axis[0])
-    plt.ylabel(axis[1])
-    if title is not None:
-        plt.title(title)
-
-    # Establecemos los límites del plot
-    xmin, xmax = np.min(X[:, 0]), np.max(X[:, 0])
-    ymin, ymax = np.min(X[:, 1]), np.max(X[:, 1])
-    scale_x = (xmax - xmin) * 0.01
-    scale_y = (ymax - ymin) * 0.01
-    plt.xlim(xmin - scale_x, xmax + scale_x)
-    plt.ylim(ymin - scale_y, ymax + scale_y)
-
-    # Mostramos scatter plot con leyenda
-    scatter = plt.scatter(X[:, 0], X[:, 1], c = y, cmap = cmap)
-    if y is not None:
-        legend1 = plt.legend(
-            *scatter.legend_elements(),
-            title = "Clases",
-            loc = "upper right")
-
-    # Pintamos las rectas con leyenda
-    if ws is not None:
-        # Elegimos los mismos colores que para los puntos
-        mask = np.ceil(np.linspace(0, len(cmap.colors) - 1, len(np.unique(y)))).astype(int)
-        colors = np.array(cmap.colors)[mask]
-
-        for w, l, c in zip(ws, labels, colors):
-            x = np.array([xmin - scale_x, xmax + scale_x])
-            plt.plot(x, (-w[0] * x) / w[1], label = l, lw = 2, ls = "--", color = c)
-
-        plt.legend(loc = "lower right")
-
-    # Añadimos leyenda sobre las clases
-    if y is not None:
-        plt.gca().add_artist(legend1)
-
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + figname + ".png")
-    else:
-        plt.show(block = False)
-
-    wait()
-
-def scatter_pca(X, y_pred):
-    """Proyección de las dos primeras componentes principales
-       con sus etiquetas predichas.
-         - X: matriz de características bidimensionales.
-         - y_pred: etiquetas predichas."""
-
-    scatter_plot(
-        X[:, [0, 1]],
-        y_pred,
-        axis = ["Primera componente principal",
-            "Segunda componente principal"],
-        title = "Proyección de las dos primeras componentes principales",
-        figname = "scatter")
-
-def scatter_pca_classes(X, y, ws, classes):
-    """Proyección de las dos primeras componentes principales
-       con sus etiquetas reales, solo para un subconjunto de clases.
-       Se muestran también los clasificadores asociados.
-         - X: matriz de características bidimensionales.
-         - y: etiquetas reales.
-         - ws: coeficientes de los clasificadores.
-         - classes: clases a representar."""
-
-    mask = np.in1d(y, classes)
-    subset_classes = np.where(mask)[0]
-    labels = ["Frontera clase {} vs all".format(i) for i in classes]
-    scatter_plot(
-        X[subset_classes, :][:, [0, 1]],
-        y[subset_classes],
-        axis = ["Primera componente principal",
-            "Segunda componente principal"],
-        ws = ws,
-        labels = labels,
-        title = "Proyección de las dos primeras componentes principales con clasificadores",
-        figname = "scatter_2",
-        cmap = cm.tab10)
-
-def confusion_matrix(clf, X, y):
-    """Muestra la matriz de confusión de un clasificador en un conjunto de datos.
-         - clf: clasificador.
-         - X, y: conjunto de datos y etiquetas."""
-
-    fig, ax = plt.subplots(1, 1, figsize = (8, 6))
-    disp = plot_confusion_matrix(clf, X, y, cmap = cm.Blues, values_format = 'd', ax = ax)
-    disp.ax_.set_title("Matriz de confusión")
-    disp.ax_.set_xlabel("Etiqueta predicha")
-    disp.ax_.set_ylabel("Etiqueta real")
-
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + "confusion.png")
-    else:
-        plt.show(block = False)
-    wait()
-
-def plot_feature_importance(importances, n, pca):
-    """Muestra las características más relevantes según el criterio
-       inferido por un RandomForest, o bien las primeras componentes principales.
-         - importances: vector de relevancia de características.
-         - n: número de características a seleccionar.
-         - pca: controla si se eligen las primeras componentes principales."""
-
-    if pca:
-        indices = range(0, n)
-        title = "Importancia de componentes principales"
-    else:
-        indices = np.argsort(importances)[-n:][::-1]
-        title = "Importancia de características"
-
-    # Diagrama de barras para la relevancia
-    plt.figure(figsize = (8, 6))
-    plt.title(title)
-    plt.xlabel("Índice")
-    plt.ylabel("Importancia")
-    plt.bar(range(n), importances[indices])
-    plt.xticks(range(n), indices)
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + "importance.png")
-    else:
-        plt.show(block = False)
-    wait()
-
-def plot_learning_curve(estimator, X, y, ylim = None, cv = None,
-                        n_jobs = None, train_sizes = np.linspace(.1, 1.0, 5)):
-    """
-    Generate 3 plots: the test and training learning curve, the training
-    samples vs fit times curve, the fit times vs score curve.
-
-    Taken from:
-    https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
-
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-
-          - None, to use the default 5-fold cross-validation,
-          - integer, to specify the number of folds.
-          - :term:`CV splitter`,
-          - An iterable yielding (train, test) splits as arrays of indices.
-
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-
-    n_jobs : int or None, optional (default=None)
-        Number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-
-    train_sizes : array-like, shape (n_ticks,), dtype float or int
-        Relative or absolute numbers of training examples that will be used to
-        generate the learning curve. If the dtype is float, it is regarded as a
-        fraction of the maximum size of the training set (that is determined
-        by the selected validation method), i.e. it has to be within (0, 1].
-        Otherwise it is interpreted as absolute sizes of the training sets.
-        Note that for classification the number of samples usually have to
-        be big enough to contain at least one sample from each class.
-        (default: np.linspace(0.1, 1.0, 5))
-    """
-
-    fig, axes = plt.subplots(1, 3, figsize = (16, 6))
-
-    axes[0].set_title("Learning Curves")
-    if ylim is not None:
-        axes[0].set_ylim(*ylim)
-    axes[0].set_xlabel("Número de ejemplos de entrenamiento")
-    axes[0].set_ylabel("Accuracy")
-
-    train_sizes, train_scores, test_scores, fit_times, _ = \
-        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-                       train_sizes=train_sizes,
-                       return_times=True)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    fit_times_mean = np.mean(fit_times, axis=1)
-    fit_times_std = np.std(fit_times, axis=1)
-
-    # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Accuracy en training")
-    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Accuracy en cross-validation")
-    axes[0].legend(loc="best")
-
-    # Plot n_samples vs fit_times
-    axes[1].grid()
-    axes[1].plot(train_sizes, fit_times_mean, 'o-')
-    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
-                         fit_times_mean + fit_times_std, alpha=0.1)
-    axes[1].set_xlabel("Número de ejemplos de entrenamiento")
-    axes[1].set_ylabel("Tiempos de entrenamiento (s)")
-    axes[1].set_title("Escalabilidad del modelo")
-
-    # Plot fit_time vs score
-    axes[2].grid()
-    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
-    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1)
-    axes[2].set_xlabel("Tiempos de entrenamiento (s)")
-    axes[2].set_ylabel("Accuracy")
-    axes[2].set_title("Desempeño del modelo")
-
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + "learning_curve.png")
-    else:
-        plt.show(block = False)
-    wait()
-
-def plot_class_distribution(y_train, y_test):
-    """Muestra la distribución de clases en entrenamiento y test."""
-
-    fig, axs = plt.subplots(1, 2, figsize = (12, 6))
-    plt.suptitle("Distribución de clases", y = 0.96)
-
-    # Diagrama de barras en entrenamiento
-    axs[0].bar(np.unique(y_train), np.bincount(y_train.astype(int)),
-        color = cm.Set3.colors)
-    axs[0].title.set_text("Entrenamiento")
-    axs[0].set_xlabel("Clases")
-    axs[0].set_ylabel("Número de ejemplos")
-    axs[0].set_xticks(range(N_CLASSES))
-
-    # Diagrama de barras en test
-    axs[1].bar(np.unique(y_test), np.bincount(y_test.astype(int)),
-        color = cm.Set3.colors)
-    axs[1].title.set_text("Test")
-    axs[1].set_xlabel("Clases")
-    axs[1].set_ylabel("Número de ejemplos")
-    axs[1].set_xticks(range(N_CLASSES))
-
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + "class_distr.png")
-    else:
-        plt.show(block = False)
-    wait()
-
-def plot_corr_matrix(raw, preproc):
-    """Muestra la matriz de correlación de un cierto conjunto, antes y
-       después del preprocesado.
-         - raw: datos antes del preprocesado.
-         - preproc: datos tras el preprocesado."""
-
-    fig, axs = plt.subplots(1, 2, figsize = (15, 6))
-    fig.suptitle("Matriz de correlación en entrenamiento", y = 0.85)
-
-    # Correlación antes de preprocesar
-    with np.errstate(invalid = 'ignore'):
-        corr_matrix = np.abs(np.corrcoef(raw, rowvar = False))
-    im = axs[0].matshow(corr_matrix, cmap = 'viridis')
-    axs[0].title.set_text("Sin preprocesado")
-
-    # Correlación tras preprocesado
-    corr_matrix_post = np.abs(np.corrcoef(preproc, rowvar = False))
-    axs[1].matshow(corr_matrix_post, cmap = 'viridis')
-    axs[1].title.set_text("Con preprocesado")
-
-    fig.colorbar(im, ax = axs.ravel().tolist(), shrink = 0.6)
-
-    if SAVE_FIGURES:
-        plt.savefig(IMG_PATH + "correlation.png")
-    else:
-        plt.show(block = False)
-    wait()
-
-#
-# PROBLEMA DE CLASIFICACIÓN EN OPTDIGITS
-#
-
 def preprocess_pipeline(selection_strategy = Selection.PCA):
-    """Construye una lista de transformaciones y parámetros para el
-       preprocesamiento de datos, con diferentes estrategias para realizar
-       selección de variables."""
+    """Construye una lista de transformaciones para el
+       preprocesamiento de datos, con transformaciones polinómicas
+       de grado 2 y diferentes estrategias para realizar selección
+       de variables."""
 
     # Reducción de dimensionalidad
     if selection_strategy == Selection.PCA:
@@ -447,10 +121,10 @@ def classification_fit(compare = False, selection_strategy = Selection.PCA, show
         print("\nMostrando gráficas sobre preprocesado...")
 
         # Mostramos distribución de clases en training y test
-        plot_class_distribution(y_train, y_test)
+        plot_class_distribution(y_train, y_test, N_CLASSES, SAVE_FIGURES, IMG_PATH)
 
         # Mostramos matriz de correlación de training antes y después de preprocesado
-        plot_corr_matrix(X_train_raw, X_train)
+        plot_corr_matrix(X_train_raw, X_train, SAVE_FIGURES, IMG_PATH)
 
     # Construimos un pipeline de selección + clasificación
     pipe_lst = []
@@ -497,18 +171,20 @@ def classification_fit(compare = False, selection_strategy = Selection.PCA, show
         print("\nMostrando gráficas sobre entrenamiento y predicción...")
 
         # Matriz de confusión
-        confusion_matrix(best_clf, X_test, y_test)
+        confusion_matrix(best_clf, X_test, y_test, SAVE_FIGURES, IMG_PATH)
 
         if show > 1 and selection_strategy is not Selection.LASSO:
             # Curva de aprendizaje
             print("Calculando curva de aprendizaje...")
-            plot_learning_curve(best_clf, X_train, y_train, n_jobs = -1)
+            plot_learning_curve(best_clf, X_train, y_train, n_jobs = -1,
+                save_figures = SAVE_FIGURES, img_path = IMG_PATH)
 
         # Importancia de características
         pipe = Pipeline(pipe_lst + [("clf", RandomForestClassifier(random_state = SEED))])
         pipe.fit(X_train, y_train)
         importances = pipe['clf'].feature_importances_
-        plot_feature_importance(importances, 10, selection_strategy == Selection.PCA)
+        plot_feature_importance(importances, 10, selection_strategy == Selection.PCA,
+            SAVE_FIGURES, IMG_PATH)
 
         # Visualización de componentes principales
         if selection_strategy == Selection.PCA:
@@ -517,14 +193,14 @@ def classification_fit(compare = False, selection_strategy = Selection.PCA, show
 
             # Proyección de las dos primeras componentes principales
             # con etiquetas predichas
-            scatter_pca(X_test, y_pred)
+            scatter_pca(X_test, y_pred, SAVE_FIGURES, IMG_PATH)
 
             # Seleccionamos dos clases concretas y mostramos también los clasificadores,
             # frente a las etiquetas reales
             classes = [1, 2, 3]
             coef = best_clf.best_estimator_['clf'].coef_
             ws = [[coef[i, 0], coef[i, 1]] for i in classes]
-            scatter_pca_classes(X_test, y_test, ws, classes)
+            scatter_pca_classes(X_test, y_test, ws, classes, SAVE_FIGURES, IMG_PATH)
 
     # Comparación con modelos no lineales
     if compare:
